e5c3b76350daf9dfa9e302078ed3e63f948185b1
gaiek da costa <pelipe.gaiek1701@gmail.com>
2022-07-10 11:11:08 -0300

    Inicio do projeto

---
+++ app.js
@@ -0,0 +1,8 @@
+const card = document.querySelectorAll('.card');
+const modalText = document.querySelector('.modal-body');
+
+// card.forEach((card) =>
+// card.addEventListener('click', (event) => {
+//     modalText.innerHTML = ``
+//    })
+//  );
