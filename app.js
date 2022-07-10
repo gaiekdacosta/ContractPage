2d045cd8fb9ce4b85f8cb28bd1680871eb557e38
gaiek da costa <104230562+gaiekdacosta@users.noreply.github.com>
2022-07-10 11:14:29 -0300

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-const card = document.querySelectorAll('.card');
-const modalText = document.querySelector('.modal-body');
-
-// card.forEach((card) =>
-// card.addEventListener('click', (event) => {
-//     modalText.innerHTML = ``
-//    })
-//  );
